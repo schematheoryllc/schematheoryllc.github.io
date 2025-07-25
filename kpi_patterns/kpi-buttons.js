7c9ef92100c6313efae6cb60c297923b53b4f354
Michael Smereka <michael.smereka@schematheoryllc.com>
2025-07-25 10:01:10 -0400

    Added KPI Revenue Churn

--- kpi_patterns/kpi-buttons.js
+++ kpi_patterns/kpi-buttons.js
@@ -19,13 +19,13 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".conventions-button").forEach((button) => {
     button.addEventListener("click", () => {
       const message = `This pattern follows these conventions:
-– Measures are created in the model, not in visuals.
-– Measures are formatted using standard formatting settings.
-– Time intelligence functions always use a Date table marked as such.
-– The Date table must have a continuous range of dates without gaps.
-– Columns in the Date table are properly related to the data model.
-– Time calculations are based on standard DAX time intelligence functions.
-– Measures are reusable and composed where possible.`;
+- Measures are created in the model, not in visuals.
+- Measures are formatted using standard formatting settings.
+- Time intelligence functions always use a Date table marked as such.
+- The Date table must have a continuous range of dates without gaps.
+- Columns in the Date table are properly related to the data model.
+- Time calculations are based on standard DAX time intelligence functions.
+- Measures are reusable and composed where possible.`;
       alert(message);
     });
   });
